401ede931562ef112f30454a60c5df6bfbcac2ab
Gabriel Tonhatti <gabrieltonhatti37@gmail.com>
2022-02-12 13:54:27 -0300

    Projeto de envio para emails com Workers

---
+++ email-worker-compose/app/app.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+pip install bottle==0.12.13 psycopg2==2.7.1
+python -u sender.py
---
+++ email-worker-compose/app/sender.py
@@ -0,0 +1,35 @@
+import psycopg2
+import os
+from bottle import route, run, request
+
+DSN = "dbname=email_sender user=postgres host=db password={}".format(
+    os.environ['DB_PASSWORD']
+)
+print(DSN)
+SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
+
+
+def register_message(assunto, mensagem):
+    conn = psycopg2.connect(DSN) 
+    cur = conn.cursor()
+    cur.execute(SQL, (assunto, mensagem))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    print('Mensagem registrada !')
+
+@route('/', method='POST')
+def send():
+    assunto = request.forms.get('assunto')
+    mensagem = request.forms.get('mensagem')
+
+    register_message(assunto, mensagem)
+
+    return 'Mensagem enfileirada ! Assunto: {} Mensagem: {}'.format(
+        assunto, mensagem
+    )
+
+
+if __name__ == '__main__':
+    run(host='0.0.0.0', port=8080, debug=True)
